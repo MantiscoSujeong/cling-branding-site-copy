fe1beb7ebc88b17ff0cdaff45f310440821c0555
MantiscoSujeong <v2pv2p@imantisco.com>
2022-10-12 10:49:30 +0900

    configuration: 초기설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import {ThemeProvider} from "styled-components";
+import {Reset} from 'styled-reset'
+import theme from "./styles/theme";
+import GlobalStyle from './styles/global';
+
+import Home from "./pages/Home";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle/>
+            <ThemeProvider theme={theme}>
+                <Reset/>
+                <Home></Home>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
